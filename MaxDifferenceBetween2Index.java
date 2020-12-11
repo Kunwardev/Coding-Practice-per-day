a1060337319c71ba9728e316ac4ea41feae22979
Kunwardev <Kunwardev@users.noreply.github.com>
2020-12-11 00:19:14 -0500

    Create MaxDifferenceBetween2Index.java
    Running two loops that will gather difference from 0 and n-1.
    Then return the max value

---
+++ MaxDifferenceBetween2Index.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Algorithms.Misc;
+
+/**
+ *
+ * @author DEV
+ */
+public class MaxDifferenceBetween2Index {
+    
+}
