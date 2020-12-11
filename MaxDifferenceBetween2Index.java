9bf07472190b7bc0e703880aa6db94e6d9d79294
Kunwardev <Kunwardev@users.noreply.github.com>
2020-12-11 00:19:56 -0500

    Update MaxDifferenceBetween2Index.java

--- MaxDifferenceBetween2Index.java
+++ MaxDifferenceBetween2Index.java
@@ -11,4 +11,27 @@ package Algorithms.Misc;
  */
 public class MaxDifferenceBetween2Index {
     
+    private static int function(int[] arr){
+        int index_1 = 0;
+        for(int i=arr.length-1;i>=0; i--){
+            if(arr[index_1] != arr[i]){
+                index_1 = i;
+                break;
+            }
+        }
+        int index_2 = arr.length-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[index_2] != arr[i]){
+                index_2 = arr.length - i - 1;
+                break;
+            }
+        }
+        return Math.max(index_1, index_2);
+    }
+    
+    public static void main(String[] args) {
+         int []a = { 1, 2, 3, 2, 3 }; 
+         System.out.println(function(a));
+    }
+    
 }
