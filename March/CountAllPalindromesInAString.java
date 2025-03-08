c460e3814a267ad15791b585c64cab5a5fcf1d0b
Kunwardev <kunwardev94@gmail.com>
2025-03-08 16:23:36 -0500

    Using Recursion to find all the palindromes from every index and then expanding both the sides

---
+++ March/CountAllPalindromesInAString.java
@@ -0,0 +1,28 @@
+package March;
+
+public class CountAllPalindromesInAString {
+    
+    private int countPalindromeFromCenter(String s, int left, int right){
+        int count = 0;
+        int length = s.length();
+        while(left >= 0 && right < length && s.charAt(left) == s.charAt(right)){
+            if(right-left+1 >= 2){
+                count++;
+            }
+            left--;
+            right++;
+        }
+        return count;
+    }
+    
+    public int countPS(String s) {
+        // code here
+        int length = s.length();
+        int totalCount = 0;
+        for(int i=0;i<length;i++){
+            totalCount += countPalindromeFromCenter(s, i, i);
+            totalCount += countPalindromeFromCenter(s, i, i+1);
+        }
+        return totalCount;
+
+}
