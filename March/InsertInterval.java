a5edf6d5612cdb79bb92d1184cef6fbc00feebff
Kunwardev <kunwardev94@gmail.com>
2025-03-13 10:43:04 -0400

    Inserting Interval with extra space

---
+++ March/InsertInterval.java
@@ -0,0 +1,50 @@
+package March;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class Interval{
+    int start;
+    int end;
+
+    public Interval(int start, int end){
+        this.start = start;
+        this.end = end;
+    }
+}
+
+public class InsertInterval {
+    
+    public List<Interval> insertInterval(List<Interval> intervals, Interval newInterval){
+        List<Interval> result = new LinkedList<>();
+        int i = 0;
+        // add all the intervals ending before newInterval starts
+        while (i < intervals.size() && intervals.get(i).end < newInterval.start)
+            result.add(intervals.get(i++));
+        // merge all overlapping intervals to one considering newInterval
+        while (i < intervals.size() && intervals.get(i).start <= newInterval.end) {
+            newInterval = new Interval( // we could mutate newInterval here also
+                    Math.min(newInterval.start, intervals.get(i).start),
+                    Math.max(newInterval.end, intervals.get(i).end));
+            i++;
+        }
+        result.add(newInterval); // add the union of intervals we got
+        // add all the rest
+        while (i < intervals.size()) result.add(intervals.get(i++)); 
+        return result;
+    }
+
+    public static void main(String[] args) {
+        InsertInterval ins = new InsertInterval();
+        List<Interval> intervals = new ArrayList<>();
+        intervals.add(new Interval(1, 2));
+        intervals.add(new Interval(4, 6));
+        Interval insert = new Interval(3, 5);
+        List<Interval> res = ins.insertInterval(intervals, insert);
+        for(Interval i: res){
+            System.out.println(i.start+ " "+i.end);
+        }
+    }
+
+}
