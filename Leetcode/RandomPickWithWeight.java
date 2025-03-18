f2985b81e0a95865bd3ce9acda05c856d3a40d13
Kunwardev <kunwardev94@gmail.com>
2025-03-18 17:48:16 -0400

    Random Pick With Weight Using Prefix Sum

--- Leetcode/RandomPickWithWeight.java
+++ Leetcode/RandomPickWithWeight.java
@@ -4,10 +4,30 @@ public class RandomPickWithWeight {
     
     private static int function(int[] arr){
         int sum = 0;
-        for(int i: arr)
-            sum += i;
-        int x = Math.round(sum);
-        
+        int[] prefixSum = new int[arr.length];
+        for(int i=0;i < arr.length;i++){
+            sum += arr[i];
+            prefixSum[i] = sum;
+        }
+        int left = 0, right = arr.length;
+        double random = Math.random()*sum;
+        System.out.println(random);
+        while(left < right){
+            int mid = (left + (right-left) / 2);
+            if(prefixSum[mid] < random){
+                left = mid+1;
+            }else{
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] weights = {1, 3, 2, 4};
+        int result = RandomPickWithWeight.function(weights);
+        System.out.println(result);
+
     }
 
 }
